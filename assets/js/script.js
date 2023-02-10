e52cda0dc3c2bd8c56ce30f3d5a02c1949972cfa
Scarlet234 <109751916+Scarlet234@users.noreply.github.com>
2023-02-09 20:53:36 -0700

    added button attributes in html and added list of genres and id numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,12 @@ var TMDKey = '9351b8541deafd7c3666f42bc7a6a545';
 var OMDKey = '25570a59';
 var movieList = [];
 
+// The genres in The Movie Data Base have id numbers: {"genres":[{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},{"id":35,"name":"Comedy"},{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},{"id":36,"name":"History"},{"id":27,"name":"Horror"},{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},{"id":878,"name":"Science Fiction"},{"id":10770,"name":"TV Movie"},{"id":53,"name":"Thriller"},{"id":10752,"name":"War"},{"id":37,"name":"Western"}]}
 
+// global event listener
+var moodButton = (clicked_id) => {
+  alert(clicked_id);
+}
 
 var getFilmsByGenre = () => {
   return JSON.parse(localStorage.getItem('filmsByGenre'))
